116d29ed63f8cde24544b40de4e4dc9548ab9bef
Deniz Kalem <denkal91@googlemail.com>
2024-10-18 19:05:47 +0200

    Specify application class name in manifest and set hilt entry point in MainActivity

--- app/src/main/java/ch/dk/sampleauthentication/core/presentation/MainActivity.kt
+++ app/src/main/java/ch/dk/sampleauthentication/core/presentation/MainActivity.kt
@@ -7,7 +7,9 @@ import androidx.activity.enableEdgeToEdge
 import androidx.navigation.compose.rememberNavController
 import ch.dk.sampleauthentication.core.presentation.navigation.AppNavigation
 import ch.dk.sampleauthentication.ui.theme.SampleAuthenticationTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
