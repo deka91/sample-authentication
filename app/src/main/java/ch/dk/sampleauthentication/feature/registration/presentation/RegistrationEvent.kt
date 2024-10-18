303ac04b714f28735f99e270687f12a5e74ae293
Deniz Kalem <denkal91@googlemail.com>
2024-10-18 17:18:40 +0200

    Create events for the user input in the registration screen

---
+++ app/src/main/java/ch/dk/sampleauthentication/feature/registration/presentation/RegistrationEvent.kt
@@ -0,0 +1,16 @@
+package ch.dk.sampleauthentication.feature.registration.presentation
+
+import androidx.compose.ui.focus.FocusState
+
+/**
+ * Created by Deniz Kalem on 18.10.2024
+ */
+sealed class RegistrationEvent {
+    data class OnNameChange(val fullName: String) : RegistrationEvent()
+    data class OnNameFocusChange(val focusState: FocusState) : RegistrationEvent()
+    data class OnEmailChange(val emailAddress: String) : RegistrationEvent()
+    data class OnEmailFocusChange(val focusState: FocusState) : RegistrationEvent()
+    data class OnBirthdayChange(val birthday: String) : RegistrationEvent()
+    data class OnBirthdayFocusChange(val focusState: FocusState) : RegistrationEvent()
+    data object OnSubmit : RegistrationEvent()
+}
