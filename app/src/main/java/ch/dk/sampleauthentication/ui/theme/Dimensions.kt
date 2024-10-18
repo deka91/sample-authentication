085243715ee3b3404535a8e72e71a11cb4340aa4
Deniz Kalem <denkal91@googlemail.com>
2024-10-18 19:03:08 +0200

    Add more dimensions value

--- app/src/main/java/ch/dk/sampleauthentication/ui/theme/Dimensions.kt
+++ app/src/main/java/ch/dk/sampleauthentication/ui/theme/Dimensions.kt
@@ -9,15 +9,29 @@ import androidx.compose.ui.unit.dp
 data class Dimensions(
     val baseline0x25: Dp,
     val baseline0x5: Dp,
+    val baseline0x75: Dp,
     val baseline1: Dp,
     val baseline1x5: Dp,
-    val baseline2: Dp
+    val baseline2: Dp,
+    val baseline2x5: Dp,
+    val baseline3: Dp,
+    val baseline3x5: Dp,
+    val baseline4: Dp,
+    val baseline4x5: Dp,
+    val baseline5: Dp
 )
 
 val DIMENSIONS = Dimensions(
     baseline0x25 = 2.dp,
     baseline0x5 = 4.dp,
+    baseline0x75 = 6.dp,
     baseline1 = 8.dp,
     baseline1x5 = 12.dp,
     baseline2 = 16.dp,
+    baseline2x5 = 20.dp,
+    baseline3 = 24.dp,
+    baseline3x5 = 28.dp,
+    baseline4 = 32.dp,
+    baseline4x5 = 36.dp,
+    baseline5 = 40.dp
 )
