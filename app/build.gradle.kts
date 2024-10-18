750b576b23a7c7f88f28cf93ee62f55f6e018301
Deniz Kalem <denkal91@googlemail.com>
2024-10-18 15:34:21 +0200

    Enable buildConfig in gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,6 +19,9 @@ android {
     }
 
     buildTypes {
+        debug {
+            isMinifyEnabled = false
+        }
         release {
             isMinifyEnabled = false
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
@@ -33,6 +36,7 @@ android {
     }
     buildFeatures {
         compose = true
+        buildConfig = true
     }
 }
 
