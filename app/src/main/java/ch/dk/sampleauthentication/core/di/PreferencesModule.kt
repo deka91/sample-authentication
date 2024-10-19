41e4eade864360294840782a14cacd0996f74288
Deniz Kalem <denkal91@googlemail.com>
2024-10-19 14:33:12 +0200

    Add Preference Module with EncryptedSharedPreferences support

---
+++ app/src/main/java/ch/dk/sampleauthentication/core/di/PreferencesModule.kt
@@ -0,0 +1,33 @@
+package ch.dk.sampleauthentication.core.di
+
+import android.content.Context
+import android.content.SharedPreferences
+import androidx.security.crypto.EncryptedSharedPreferences
+import androidx.security.crypto.MasterKey
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object PreferencesModule {
+
+    @Singleton
+    @Provides
+    fun provideEncryptedSharedPreferences(@ApplicationContext appContext: Context): SharedPreferences {
+        val masterKey = MasterKey.Builder(appContext, MasterKey.DEFAULT_MASTER_KEY_ALIAS)
+            .setKeyScheme(MasterKey.KeyScheme.AES256_GCM)
+            .build()
+
+        return EncryptedSharedPreferences.create(
+            appContext,
+            "sample_auth_prefs",
+            masterKey,
+            EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+            EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
+        )
+    }
+}
