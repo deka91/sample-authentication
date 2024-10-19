f55fa57b73bc98897b6c3da8a71fdc2327502a84
Deniz Kalem <denkal91@googlemail.com>
2024-10-19 08:33:43 +0200

    Add new registration event to handle error messages

--- app/src/main/java/ch/dk/sampleauthentication/feature/registration/presentation/RegistrationEvent.kt
+++ app/src/main/java/ch/dk/sampleauthentication/feature/registration/presentation/RegistrationEvent.kt
@@ -6,11 +6,12 @@ import androidx.compose.ui.focus.FocusState
  * Created by Deniz Kalem on 18.10.2024
  */
 sealed class RegistrationEvent {
-    data class OnNameChange(val fullName: String) : RegistrationEvent()
+    data class OnNameChange(val name: String) : RegistrationEvent()
     data class OnNameFocusChange(val focusState: FocusState) : RegistrationEvent()
     data class OnEmailChange(val emailAddress: String) : RegistrationEvent()
     data class OnEmailFocusChange(val focusState: FocusState) : RegistrationEvent()
     data class OnBirthdayChange(val birthday: String) : RegistrationEvent()
     data class OnBirthdayFocusChange(val focusState: FocusState) : RegistrationEvent()
+    data object OnErrorMessageSeen : RegistrationEvent()
     data object OnSubmit : RegistrationEvent()
 }
