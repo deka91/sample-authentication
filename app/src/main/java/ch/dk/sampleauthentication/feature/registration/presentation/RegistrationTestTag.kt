c5b75098fc4e104f85e3db7c12ac6770a06d7f0e
Deniz Kalem <denkal91@googlemail.com>
2024-10-19 21:39:14 +0200

    Add constants for registration test tags

---
+++ app/src/main/java/ch/dk/sampleauthentication/feature/registration/presentation/RegistrationTestTag.kt
@@ -0,0 +1,13 @@
+package ch.dk.sampleauthentication.feature.registration.presentation
+
+/**
+ * Created by michael on 25.05.2023.
+ * BLS AG
+ * michael.ruppen@bls.ch
+ */
+object RegistrationTestTag {
+    const val INPUT_NAME = "registration_input_name"
+    const val INPUT_EMAIL = "registration_input_email"
+    const val INPUT_BIRTHDAY = "registration_input_birthday"
+    const val BUTTON_SUBMIT = "registration_button_submit"
+}
